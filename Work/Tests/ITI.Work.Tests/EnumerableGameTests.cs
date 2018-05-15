e0ba6731698f33cc8a396e5de3cbed4d98fe47c7
Julie <lacognata@intechinfo.fr>
2018-05-15 12:24:01 +0200

    Modify EnumerableGameTests

--- Work/Tests/ITI.Work.Tests/EnumerableGameTests.cs
+++ Work/Tests/ITI.Work.Tests/EnumerableGameTests.cs
@@ -11,18 +11,59 @@ namespace ITI.Work.Tests
     public class EnumerableGameTests
     {
 
-        IEnumerable<long> GetNumbers( int start, int count )
+        IEnumerable<long> GetNumbers( long start, long count )
         {
+            long idx = 0;
+
+            while(idx < count)
+            {
+                yield return start + idx;
+                idx++;
+            }
+
+            // Vision du prof : for (long i = 0; i < count; ++i) yield return start + i;
         }
 
-        IEnumerable<long> GetNumbersThatAreNotMultipleOf( int start, int count, IEnumerable<long> divisors )
+        IEnumerable<long> GetNumbersThatAreNotMultipleOf( long start, long count, IEnumerable<long> divisors )
         {
+            long idx = 0;
+
+            while( idx < count )
+            {
+                foreach (long i in divisors)
+                {
+                    if( idx % i == 0 ) yield return start + idx;
+                }
+
+                idx++;
+            }
         }
 
         [Test]
         public void generating_numbers()
         {
+            IEnumerable<long> test = GetNumbers( 1, 5 );
+            long t = 1;
 
+            foreach( long i in test )
+            {
+                Assert.AreEqual( i, t );
+                t++;
+            }
+        }
+
+        [Test]
+        public void generating_numbers_that_are_not_multiple_of()
+        {
+            IEnumerable<long> test = GetNumbers( 2, 4 );
+            IEnumerable<long> getMultipleOf = GetNumbersThatAreNotMultipleOf( 0, 10, test );
+
+            long x = 0;
+
+            foreach( long i in getMultipleOf )
+            {
+                Assert.AreEqual( i, x );
+            }
         }
     }
 }
